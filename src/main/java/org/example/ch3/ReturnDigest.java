925cf662c351cee695ab8d265058254729fe7f3f
남의영 <ny2485@naver.com>
2024-01-13 15:14:20 +0900

    thread synchronization issues

--- src/main/java/org/example/ch3/ReturnDigest.java
+++ src/main/java/org/example/ch3/ReturnDigest.java
@@ -25,6 +25,13 @@ public class ReturnDigest extends Thread {
             while(din.read() != -1);
             din.close();
             digest = sha.digest();
+
+            /** 공유자원으로써 동기화 문제 부분 */
+            System.out.println("input : ");
+            System.out.println(DatatypeConverter.printHexBinary(digest));
+            System.out.println();
+            /** end */
+
         } catch (IOException | NoSuchAlgorithmException e) {
             System.out.println(e);
         }
