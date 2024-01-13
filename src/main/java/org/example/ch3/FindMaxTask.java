5fe5233892420f4b636329cb36cc92cf5b0e0786
남의영 <ny2485@naver.com>
2024-01-13 14:58:12 +0900

    Future, Callable, Executor use exam

--- src/main/java/org/example/ch3/FindMaxTask.java
+++ src/main/java/org/example/ch3/FindMaxTask.java
@@ -1,2 +1,24 @@
-package org.example.ch3;public class FindMaxTask {
+package org.example.ch3;
+
+import java.util.concurrent.Callable;
+
+public class FindMaxTask implements Callable<Integer> {
+
+    private int[] data;
+    private int start;
+    private int end;
+
+    public FindMaxTask(int[] data, int start, int end) {
+        this.data = data;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Integer call() {
+        int max = Integer.MIN_VALUE;
+        for (int i = start; i < end; i++) {
+            if(data[i]>max) max = data[i];
+        }
+        return max;
+    }
 }
--- src/main/java/org/example/ch3/MultithreadedMaxFinder.java
+++ src/main/java/org/example/ch3/MultithreadedMaxFinder.java
@@ -1,2 +1,30 @@
-package org.example.ch3;public class MultithreadedMaxFinder {
+package org.example.ch3;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MultithreadedMaxFinder {
+
+    public static int max(int[] data) throws InterruptedException, ExecutionException {
+
+        if (data.length == 1) {
+            return data[0];
+        } else if (data.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        //작업을 둘로 분할
+        FindMaxTask task1 = new FindMaxTask(data, 0, data.length / 2);
+        FindMaxTask task2 = new FindMaxTask(data, data.length / 2, data.length);
+
+        //두개의 스레드 생성
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        Future<Integer> future1 = service.submit(task1);
+        Future<Integer> future2 = service.submit(task2);
+
+        return Math.max(future1.get(), future2.get());
+    }
 }
